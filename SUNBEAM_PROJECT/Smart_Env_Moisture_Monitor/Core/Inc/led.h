626e409b1b8a0a59796ca5273312e88b5cc2bb0e
Pranjal Dudhal <dudhalpranjal@gmail.com>
2026-01-02 09:49:00 +0530

    SUNBEAM_PROJECT

---
+++ SUNBEAM_PROJECT/Smart_Env_Moisture_Monitor/Core/Inc/led.h
@@ -0,0 +1,19 @@
+/*
+ * led.h
+ *
+ *  Created on: Jan 02, 2025
+ *      Author: ADMIN
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+
+#include "main.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+void Led_Alert(void);
+
+#endif /* INC_LED_H_ */
